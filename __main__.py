0a749046490b2bf0c29d5bc17c0d020331838c6b
Shivam Mishra <s.kmishra703@gmail.com>
2020-12-08 18:42:57 +0530

    phate k changes

--- __main__.py
+++ __main__.py
@@ -13,9 +13,7 @@ from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton
 load_dotenv("config.env")
 bot = telebot.TeleBot(os.environ.get("TELEGRAM_TOKEN")) or None
 users_dict = {}
-
-
-
+poll_dict = {}
 
 
 #### Front page for loggedin User
@@ -26,6 +24,8 @@ def front_page(user, message):
     markup.add(InlineKeyboardButton("Aviral marks", callback_data="aviral"),
                                     InlineKeyboardButton("New announcements", callback_data="announcement"))
     markup.add(InlineKeyboardButton("Online Class links", callback_data="classlinks"))
+    markup.add(InlineKeyboardButton("Create Poll", callback_data = "createpoll"), 
+                                     InlineKeyboardButton("Active Poll", callback_data = "activepoll"))
     if(user.is_admin):
         bot.send_message(message.chat.id, "Awesome you are an admin!!!")
         markup.add(InlineKeyboardButton("Admin Panel", callback_data="admin_panel"))
@@ -106,6 +106,13 @@ def callback_query(call):
     elif call.data == "sure_a":
         bot.answer_callback_query(call.id)
         admin.broadcast(bot, users_dict)
+    elif call.data == "createpoll":
+        bot.answer_callback_query(call.id)
+        if call.message.chat.id in users_dict:
+            bot.register_next_step_handler(call.message, create_poll)
+    elif call.data == "activepoll":
+        if call.message.chat.id in users_dict:
+            bot.register_next_step_handler(call.message, get_active_poll)
 
 
 #### Handler functions
@@ -130,6 +137,90 @@ def trial_setup(user):
     admins = ['mit2020122', 'mit2020080']
     if(user.username in admins):
         user.is_admin = True
+
+def get_poll_result(message):
+    user = users_dict[message.chat.id]
+    current_poll = user.current_poll
+    options = current_poll.options
+    response = current_poll.response
+    total_response = current_poll.total_response
+    for index, resp_count in current_poll.response.items():
+        bot.send_message(message, str(options[index])+ " - " + str((response[index]/total_reponse)*100))
+    user.current_poll = None
+
+
+def get_response(message):
+    response = message.text
+    user = users_dict[message.chat.id]
+    current_poll = user.current_poll
+    if user.answered_poll is  not None:
+        user.answered_poll.append(user.current_poll)
+    else:
+        user.answered_poll = list(user.current_poll)
+    current_poll.response[response] += 1
+    current_poll.total_response += 1
+    bot.register_next_step_handler(message, get_poll_result)
+
+
+def display_poll(message):
+    user = users_dict[message.chat.id]
+    current_poll = user.current_poll
+    answered_poll = user.answered_poll
+    bot.send_message(message.chat.id, current_poll.question)
+    for index, option in current_poll.options.items():
+        bot.send_message(message.chat.id, str(index)+" - "+ str(option))
+    if  current_poll in answered_poll:
+         bot.register_next_step_handler(message, get_poll_result)
+    else:
+         bot.reply_to(message, "Enter your response")
+         bot.register_next_step_handler(message, get_response)
+
+
+def get_active_poll(message):
+    user = users_dict[message.chat.id]
+    active_polls = {}
+    counter = 1
+    for id in poll_dict.values():
+        active_polls[counter] = id
+        counter += 1
+    for index, poll in active_polls.items():
+        bot.send_message(message.chat.id, str(index)+"-"+str(poll.about_poll))
+    bot.send_message(message.chat.id, "Choose poll")
+    poll_index = message.text
+    user.current_poll = active_poll[poll_index]
+    bot.register_next_step_handler(message, display_poll)
+
+def get_option(message, option_no):
+    bot.send_message(message.chat.id, "Enter option "+str(option_no))
+    option = message.text
+    return option
+
+def get_poll_question(message):
+    question = message.text
+    poll = poll_dict[message.chat.id]
+    poll.question = question
+    bot.reply_to(message, "Enter number of options")
+    number_of_option = message.text+1
+    options = {}
+    response = {}
+    for i in range(1, number_of_option):
+        options[i] = get_option(message, i)
+        response[i] = 0
+    poll.options = options
+    poll.response = response
+
+def get_about_poll(message):
+    about_poll = message.text
+    poll = poll_dict[message.chat.id]
+    poll.about_poll = about_poll
+    bot.reply_to(message, "Enter poll question")
+    bot.register_next_step_handler(message, get_poll_question)
+
+def create_poll(message):
+    new_poll = NewPoll()
+    poll_dict[message.chat.id] = new_poll
+    bot.send_message(message.chat.id, "Enter briefly  about poll")
+    bot.regiester_next_step_handler(message, get_about_poll)
 
 def get_username(message):
     username = message.text
@@ -191,3 +282,4 @@ def message_handler(message):
 
 #### BOT STARTING POINT
 bot.polling(none_stop=True)
+
--- modules/data.py
+++ modules/data.py
@@ -16,6 +16,17 @@ class AnnouncementUpdate:
     soup: str = ""
     update_id : int = 0
 
+class PollData:
+    def __init__(self):
+        self.id = 0
+        self.active_polls = None
+        self.active_poll_count = None
+        self.question = None   # poll question corresponding to each poll id
+        self.options  = None
+        self.response = None
+        self.about_poll = None      # stores the data why this poll is conducted
+
+
 class User:
     def __init__(self, username):
         self.name = None
@@ -26,7 +37,8 @@ class User:
         self.cs_token = None
         self.request_no = 0
         self.is_admin = False
-
+        self.current_poll = None
+        self.answered_poll = None   # stores the object of all polls answered by user
     def get_announcement(self) -> dict:
         if self.request_no == 5:
            self.request_no = 0
