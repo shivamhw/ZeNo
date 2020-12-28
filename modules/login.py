2ae997cb663f0cca7f547921d9160c387e46e8ce
Shivam Mishra <s.kmishra703@gmail.com>
2020-12-28 14:27:33 +0530

    poll creation removed

--- modules/login.py
+++ modules/login.py
@@ -57,9 +57,8 @@ def front_page(user, message):
     markup.add(InlineKeyboardButton("Aviral marks", callback_data="aviral"),
                InlineKeyboardButton("New announcements", callback_data="announcement"))
     markup.add(InlineKeyboardButton("Online Class links", callback_data="classlinks"))
-    markup.add(InlineKeyboardButton("Create Poll", callback_data="create_poll"),
-               InlineKeyboardButton("Show Active Poll", callback_data="show_poll"),
-               InlineKeyboardButton("Logout", callback_data="logout"))
+    markup.add(InlineKeyboardButton("Show Active Poll", callback_data="show_poll"),
+               InlineKeyboardButton("Logout", callback_data="logout"))   #InlineKeyboardButton("Create Poll", callback_data="create_poll"),
     markup.row_width = 2
     markup.add(
                InlineKeyboardButton("About", callback_data="about"),
