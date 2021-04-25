38b3be79fa3e4e9bcb52d0ab952975ccadc871ad
Shivam Mishra <s.kmishra703@gmail.com>
2021-04-25 13:28:41 +0530

    moved is_reg to helper

--- __main__.py
+++ __main__.py
@@ -2,7 +2,7 @@ from modules import admin, classlinks, polls
 from ZeNo import bot, users_dict, poll_dict
 from modules.login import front_page
 from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton
-from modules.dbhelper import is_in_db, get_user_db
+from modules.helper import is_reg
 import time
 import os
 import subprocess
@@ -18,17 +18,8 @@ def gen_markup_login():
                InlineKeyboardButton("FAQ", callback_data="faq"))
     return markup
 
-def is_reg(message):
-    if(message.chat.id in users_dict):
-        return True
-    elif(is_in_db(message.chat.id)):
-        bot.send_message(message.chat.id, "not in cache but trying to get it from db....")
-        user = get_user_db(message.chat.id)
-        users_dict[message.chat.id] = user
-        return True
-    return False
 
-### Handlers
+### Command Handlers
 @bot.message_handler(commands=['start'])
 def message_handler(message):
     hello_msg = ("Hey, I am ZeNo. I was created to save your time so you can do your assignments with one hand!\n"
@@ -38,14 +29,14 @@ def message_handler(message):
         bot.send_message(message.chat.id, "you are registered with us!")
         front_page(users_dict[message.chat.id], message)
     else:
-        bot.send_message(message.chat.id, "ohhh! you are not registered with us.\nWant to login??", reply_markup=gen_markup_login())
+        bot.send_message(message.chat.id, "ohhh! you are not logged in!", reply_markup=gen_markup_login())
 
 @bot.message_handler(commands=['hi'])
 def message_handler(message):
     if(is_reg(message)):
         front_page(users_dict[message.chat.id], message)
     else:
-        bot.send_message(message.chat.id, "ohhh! you are not registered with us.\nWant to login??", reply_markup=gen_markup_login())
+        bot.send_message(message.chat.id, "ohhh! you are not logged in!", reply_markup=gen_markup_login())
 
 
 #### BOT STARTING POINT
@@ -53,5 +44,7 @@ def message_handler(message):
 try:
     time.sleep(2)
     bot.polling(none_stop=True)
-except:
-    subprocess.call(['bash', './reset.sh', os.getcwnd()])
+except Exception as e:
+    print(str(e))
+    cur = os.getcwd()+'/ZeNo'
+    subprocess.call(['bash', cur+'/reset.sh', cur])
